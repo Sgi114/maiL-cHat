6d4882ef3b4e4bb135d735c12bb497c061bf82a2
sgi <ppppk3413@gmail.com>
2024-03-20 22:10:47 +0900

    feat: enough mailを追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,239 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         // This is the theme of your application.
+//         //
+//         // TRY THIS: Try running your application with "flutter run". You'll see
+//         // the application has a purple toolbar. Then, without quitting the app,
+//         // try changing the seedColor in the colorScheme below to Colors.green
+//         // and then invoke "hot reload" (save your changes or press the "hot
+//         // reload" button in a Flutter-supported IDE, or press "r" if you used
+//         // the command line to start the app).
+//         //
+//         // Notice that the counter didn't reset back to zero; the application
+//         // state is not lost during the reload. To reset the state, use hot
+//         // restart instead.
+//         //
+//         // This works for code too, not just values: Most code changes can be
+//         // tested with just a hot reload.
+//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+//         useMaterial3: true,
+//       ),
+//       home: const MyHomePage(title: 'Flutter Demo Home Page'),
+//     );
+//   }
+// }
+
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({super.key, required this.title});
+
+//   // This widget is the home page of your application. It is stateful, meaning
+//   // that it has a State object (defined below) that contains fields that affect
+//   // how it looks.
+
+//   // This class is the configuration for the state. It holds the values (in this
+//   // case the title) provided by the parent (in this case the App widget) and
+//   // used by the build method of the State. Fields in a Widget subclass are
+//   // always marked "final".
+
+//   final String title;
+
+//   @override
+//   State<MyHomePage> createState() => _MyHomePageState();
+// }
+
+// class _MyHomePageState extends State<MyHomePage> {
+//   int _counter = 0;
+
+//   void _incrementCounter() {
+//     setState(() {
+//       // This call to setState tells the Flutter framework that something has
+//       // changed in this State, which causes it to rerun the build method below
+//       // so that the display can reflect the updated values. If we changed
+//       // _counter without calling setState(), then the build method would not be
+//       // called again, and so nothing would appear to happen.
+//       _counter++;
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     // This method is rerun every time setState is called, for instance as done
+//     // by the _incrementCounter method above.
+//     //
+//     // The Flutter framework has been optimized to make rerunning build methods
+//     // fast, so that you can just rebuild anything that needs updating rather
+//     // than having to individually change instances of widgets.
+//     return Scaffold(
+//       appBar: AppBar(
+//         // TRY THIS: Try changing the color here to a specific color (to
+//         // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+//         // change color while the other colors stay the same.
+//         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+//         // Here we take the value from the MyHomePage object that was created by
+//         // the App.build method, and use it to set our appbar title.
+//         title: Text(widget.title),
+//       ),
+//       body: Center(
+//         // Center is a layout widget. It takes a single child and positions it
+//         // in the middle of the parent.
+//         child: Column(
+//           // Column is also a layout widget. It takes a list of children and
+//           // arranges them vertically. By default, it sizes itself to fit its
+//           // children horizontally, and tries to be as tall as its parent.
+//           //
+//           // Column has various properties to control how it sizes itself and
+//           // how it positions its children. Here we use mainAxisAlignment to
+//           // center the children vertically; the main axis here is the vertical
+//           // axis because Columns are vertical (the cross axis would be
+//           // horizontal).
+//           //
+//           // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+//           // action in the IDE, or press "p" in the console), to see the
+//           // wireframe for each widget.
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             const Text(
+//               'You have pushed the button this many times:',
+//             ),
+//             Text(
+//               '$_counter',
+//               style: Theme.of(context).textTheme.headlineMedium,
+//             ),
+//           ],
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: _incrementCounter,
+//         tooltip: 'Increment',
+//         child: const Icon(Icons.add),
+//       ), // This trailing comma makes auto-formatting nicer for build methods.
+//       // リセットボタン追加して
+//       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat, 
+//       persistentFooterButtons: <Widget>[
+//         FloatingActionButton(
+//           onPressed: () {
+//             setState(() {
+//               _counter = 0;
+//             });
+//           },
+//           tooltip: 'Reset',
+//           child: const Icon(Icons.refresh),
+//         ),
+//       ],
+//     );
+//   }
+// }
+
+import 'dart:io';
+import 'package:enough_mail/enough_mail.dart';
+
+String userName = 'user.name';
+String password = 'password';
+String domain = 'example.com';
+
+void main() async {
+  await mailExample();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+/// Builds a simple example message
+MimeMessage buildMessage() {
+  final builder = MessageBuilder.prepareMultipartAlternativeMessage(
+    plainText: 'Hello world!',
+    htmlText: '<p>Hello world!</p>',
+  )
+    ..from = [MailAddress('Personal Name', 'sender@domain.com')]
+    ..to = [
+      MailAddress('Recipient Personal Name', 'recipient@domain.com'),
+      MailAddress('Other Recipient', 'other@domain.com')
+    ];
+  return builder.buildMimeMessage();
+}
+
+/// Builds an example message with attachment
+Future<MimeMessage> buildMessageWithAttachment() async {
+  final builder = MessageBuilder()
+    ..from = [MailAddress('Personal Name', 'sender@domain.com')]
+    ..to = [
+      MailAddress('Recipient Personal Name', 'recipient@domain.com'),
+      MailAddress('Other Recipient', 'other@domain.com')
+    ]
+    ..addMultipartAlternative(
+      plainText: 'Hello world!',
+      htmlText: '<p>Hello world!</p>',
     );
+  final file = File.fromUri(Uri.parse('file://./document.pdf'));
+  await builder.addFile(file, MediaSubtype.applicationPdf.mediaType);
+  return builder.buildMimeMessage();
+}
+
+/// High level mail API example
+Future<void> mailExample() async {
+  final email = '$userName@$domain';
+  print('discovering settings for  $email...');
+  final config = await Discover.discover(email);
+  if (config == null) {
+    // note that you can also directly create an account when
+    // you cannot auto-discover the settings:
+    // Compare the [MailAccount.fromManualSettings]
+    // and [MailAccount.fromManualSettingsWithAuth]
+    // methods for details.
+    print('Unable to auto-discover settings for $email');
+    return;
   }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  print('connecting to ${config.displayName}.');
+  final account =
+      MailAccount.fromDiscoveredSettings(name:'my account',userName:'my account', email:email, password:password, config:config);
+  final mailClient = MailClient(account, isLogEnabled: true);
+  try {
+    await mailClient.connect();
+    print('connected');
+    final mailboxes =
+        await mailClient.listMailboxesAsTree(createIntermediate: false);
+    print(mailboxes);
+    await mailClient.selectInbox();
+    final messages = await mailClient.fetchMessages(count: 20);
+    messages.forEach(printMessage);
+    mailClient.eventBus.on<MailLoadEvent>().listen((event) {
+      print('New message at ${DateTime.now()}:');
+      printMessage(event.message);
     });
+    await mailClient.startPolling();
+    // generate and send email:
+    final mimeMessage = buildMessage();
+    await mailClient.sendMessage(mimeMessage);
+  } on MailException catch (e) {
+    print('High level API failed with $e');
   }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+}
+void printMessage(MimeMessage message) {
+  print('from: ${message.from} with subject "${message.decodeSubject()}"');
+  if (!message.isTextPlainMessage()) {
+    print(' content-type: ${message.mediaType}');
+  } else {
+    final plainText = message.decodeTextPlainPart();
+    if (plainText != null) {
+      final lines = plainText.split('\r\n');
+      for (final line in lines) {
+        if (line.startsWith('>')) {
+          // break when quoted text starts
+          break;
+        }
+        print(line);
+      }
+    }
   }
 }
